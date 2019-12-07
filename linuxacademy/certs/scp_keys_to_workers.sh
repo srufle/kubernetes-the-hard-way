d68b645e842de17de65de01cef2545a70563e685
Stephen Rufle <srufle@gmail.com>
2019-12-07 07:01:43 -0700

    Moved worker files to certs dir from specific named dirs

--- linuxacademy/certs/scp_keys_to_workers.sh
+++ linuxacademy/certs/scp_keys_to_workers.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 source "../playground.env"
 
-$SCP ca.pem "worker1/${WORKER1_HOST}-key.pem" "worker1/${WORKER1_HOST}.pem" "${WORKER1_HOST}:~/"
-$SCP ca.pem "worker2/${WORKER2_HOST}-key.pem" "worker2/${WORKER2_HOST}.pem" "${WORKER2_HOST}:~/"
+$SCP ca.pem "${WORKER1_HOST}-key.pem" "${WORKER1_HOST}.pem" "${WORKER1_HOST}:~/"
+$SCP ca.pem "${WORKER2_HOST}-key.pem" "${WORKER2_HOST}.pem" "${WORKER2_HOST}:~/"
