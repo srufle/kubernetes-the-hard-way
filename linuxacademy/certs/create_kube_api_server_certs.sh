97059b40a2b218011b4e4ba62171715d48f2b718
Stephen Rufle <srufle@gmail.com>
2019-12-06 22:11:26 -0700

    Adding vars file and api server certs script

---
+++ linuxacademy/certs/create_kube_api_server_certs.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+source "../playground.env"
+
+CERT_HOSTNAME=10.32.0.1,${CONTROLLER1_IP},${CONTROLLER1_HOST},${CONTROLLER2_IP},${CONTROLLER2_HOST},${API_SERVER_IP},${API_SERVER_HOST},127.0.0.1,localhost,kubernetes.default
+
+{
+
+cat > kubernetes-csr.json << EOF
+{
+  "CN": "kubernetes",
+  "key": {
+    "algo": "rsa",
+    "size": 2048
+  },
+  "names": [
+    {
+      "C": "US",
+      "L": "Portland",
+      "O": "Kubernetes",
+      "OU": "Kubernetes The Hard Way",
+      "ST": "Oregon"
+    }
+  ]
+}
+EOF
+
+cfssl gencert \
+  -ca=ca.pem \
+  -ca-key=ca-key.pem \
+  -config=ca-config.json \
+  -hostname=${CERT_HOSTNAME} \
+  -profile=kubernetes \
+  kubernetes-csr.json | cfssljson -bare kubernetes
+
+}
